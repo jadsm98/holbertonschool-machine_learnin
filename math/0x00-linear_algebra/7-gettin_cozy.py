13f5074eff50005574c799520c8cd62b70267cd9
jadsm98 <jadsamaha98@gmail.com>
2020-12-03 23:21:58 +0000

    cozy

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -16,6 +16,6 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if len(mat1) != len(mat2):
         return None
 
-    for i in range(len(mat1a)):
-        mat3.append(mat1a[i] + mat2a[i])
+    for i in range(len(mat1)):
+        mat3.append(mat1[i] + mat2[i])
     return mat3
