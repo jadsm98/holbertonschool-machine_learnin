3b0f38463417b0434c183145e5cfd4211d5bfca2
jadsm98 <jadsamaha98@gmail.com>
2020-12-03 17:43:16 +0000

    matmul

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -24,5 +24,5 @@ def mat_mul(mat1, mat2):
     for i in range(len(mati)):
         for j in range(len(matj[0])):
             for k in range(len(matj)):
-                mat3[i][j] += mat1[]][k]*mat2[k][j]
+                mat3[i][j] += mat1[i][k]*mat2[k][j]
     return mat3
