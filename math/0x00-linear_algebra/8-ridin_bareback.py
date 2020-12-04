a97bf3ac259a9938094f8ebbdf22d126a4b3f115
jadsm98 <jadsamaha98@gmail.com>
2020-12-04 20:36:16 +0000

    mult

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -9,20 +9,12 @@ def mat_mul(mat1, mat2):
     function
     """
 
-    if len(mat1) != len(mat2[0]) and len(mat1[0]) != len(mat2):
+    if len(mat1[0]) != len(mat2):
         return None
+    mat3 = [[0 for i in mat2[0]] for j in mat1]
 
-    if len(mat1) == len(mat2[0]):
-        mati = mat2
-        matj = mat1
-    else:
-        mati = mat1
-        matj = mat2
-
-    mat3 = [[0 for i in matj[0]] for j in mati]
-
-    for i in range(len(mati)):
-        for j in range(len(matj[0])):
-            for k in range(len(matj)):
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
                 mat3[i][j] += mat1[i][k]*mat2[k][j]
     return mat3
