87ca89d0e47fb9dd3dfc8c0704056706ffa559a6
jadsm98 <jadsamaha98@gmail.com>
2020-12-04 20:39:46 +0000

    concat

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -12,6 +12,4 @@ def np_cat(mat1, mat2, axis=0):
     function
     """
 
-    if not axis:
-        return np.vstack((mat1, mat2))
-    return np.hstack((mat1, mat2))
+    return np.concatenate((mat1, mat2), axis=axis)
