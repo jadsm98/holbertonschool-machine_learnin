a3cd422e474f491c3eb237ee6b8fa9c36b28c3cb
jadsm98 <jadsamaha98@gmail.com>
2021-01-05 08:28:36 +0000

    neural network

--- supervised_learning/0x01-classification/9-neural_network.py
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -28,31 +28,31 @@ class NeuralNetwork:
         self.__A2 = 0
 
     @property
-    def W1:
+    def W1(self):
         """getter"""
         return self.__W1
 
     @property
-    def b1:
+    def b1(self):
         """getter"""
         return self.__b1
 
     @property
-    def A1:
+    def A1(self):
         """getter"""
         return self.__A1
 
     @property
-    def W2:
+    def W2(self):
         """getter"""
         return self.__W2
 
     @property
-    def b2:
+    def b2(self):
         """getter"""
         return self.__b2
 
     @property
-    def A2:
+    def A2(self):
         """getter"""
         return self.__A2
