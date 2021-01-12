dd5cdb55b84ede41973c4769385748e29beeabc3
jadsm98 <jadsamaha98@gmail.com>
2021-01-12 21:15:42 +0000

    onw hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -7,6 +7,10 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """function"""
+    if type(Y) is not np.ndarray or len(Y) == 0:
+        return None
+    if type(classes) is not int or classes <= np.max(Y):
+        return None
     encode = np.zeros([classes, Y.shape[0]])
     for i in range(classes):
         for j in range(Y.shape[0]):
