a02b7b601befe7f1be9befeca17bc2bc173f5181
jadsm98 <jadsamaha98@gmail.com>
2021-01-12 07:36:41 +0000

    neural network

--- supervised_learning/0x01-classification/13-neural_network.py
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -92,12 +92,12 @@ class NeuralNetwork:
         """
         m = len(Y[0])
         dz2 = A2 - Y
-        dw2 = (np.matmul(dz2, A1.T))/m
-        db2 = (np.sum(dz2, axis=1, keepdims=True))/m
-        dz1 = np.multiply(np.matmul(self.__W2.T, dz2), A1*(1 - A1))
-        dw1 = (np.matmul(dz1, X.T))/m
-        db1 = (np.sum(dz1, axis=1, keepdims=True))/m
-        self.__W1 -= alpha*dw1
-        self.__W2 -= alpha*dw2
-        self.__b1 -= alpha*db1
-        self.__b2 -= alpha*db2
+        dw2 = (np.matmul(dz2, A1.T)) / m
+        db2 = (np.sum(dz2, axis=1, keepdims=True)) / m
+        dz1 = np.matmul(self.__W2.T, dz2) * (A1 * (1 - A1))
+        dw1 = (np.matmul(dz1, X.T)) / m
+        db1 = (np.sum(dz1, axis=1, keepdims=True)) / m
+        self.__W1 -= alpha * dw1
+        self.__W2 -= alpha * dw2
+        self.__b1 -= alpha * db1
+        self.__b2 -= alpha * db2
