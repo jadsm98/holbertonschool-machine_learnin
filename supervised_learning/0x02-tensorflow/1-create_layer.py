4cfbe5c339ad5300e8b8dea1b58936ef3df2b716
jadsm98 <jadsamaha98@gmail.com>
2021-01-14 12:44:20 +0000

    layer

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -8,6 +8,5 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """function"""
     weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    l = tf.layers.dense(units=n, activation=activation,
-                        kernel_initializer=weights, name="layer")
-    return l(prev)
+    return tf.layers.Dense(inputs=prev, units=n, activation=activation,
+                           kernel_initializer=weights, name="layer")
