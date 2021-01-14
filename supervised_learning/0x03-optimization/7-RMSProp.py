aefe543717325f187fc216eb787f65b63719fa0a
jadsm98 <jadsamaha98@gmail.com>
2021-01-14 19:27:31 +0000

    optimization

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -4,6 +4,6 @@
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """function"""
-    Sv = s * beta2 + (1 - beta2) * grad**2
-    update = var - alpha * grad /(Sv**0.5 + epsilon)
+    Sv = s * beta2 + (1 - beta2) * grad ** 2
+    update = var - alpha * grad / (Sv** 0.5) + epsilon)
     return update, Sv
