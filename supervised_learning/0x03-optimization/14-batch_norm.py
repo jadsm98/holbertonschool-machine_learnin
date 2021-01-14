be07ee56cdab8a83afe08302c59f36dedd8e9c98
jadsm98 <jadsamaha98@gmail.com>
2021-01-14 20:38:33 +0000

    optimization

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -10,8 +10,8 @@ def create_batch_norm_layer(prev, n, activation):
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layers = tf.layers.Dense(units=n, kernel_initializer=k_init)
     z = layers(prev)
-    gamma = tf.Variable(initial_value=tf.constant(1.0, shape=[n]))
-    beta = tf.Variable(initial_value=tf.constant(1.0, shape=[n]))
+    gamma = tf.Variable(initial_value=tf.constant(1.0, shape=[n]), trainable=True)
+    beta = tf.Variable(initial_value=tf.constant(1.0, shape=[n]), trainable=True)
     mean, var = tf.nn.moments(z, axes=0)
     norm = tf.nn.batch_normalization(z, mean, var, offset=beta,
                                      scale=gamma, variance_epsilon=1e-8)
