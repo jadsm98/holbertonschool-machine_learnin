c0927b5bc8e4eec3ad05bd4ac6a2441d97e1c2d7
jadsm98 <jadsamaha98@gmail.com>
2021-01-14 19:19:21 +0000

    optimization

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -7,4 +7,4 @@ import tensorflow as tf
 
 def create_momentum_op(loss, alpha, beta1):
     """function"""
-    return tf.train.MomentumOptimizer(alpha, beta).minimize(loss)
+    return tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
