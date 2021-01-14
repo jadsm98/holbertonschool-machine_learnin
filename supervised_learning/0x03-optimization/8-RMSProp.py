63e8923ea7c54f1ea5a95ec89eb9a67946499223
jadsm98 <jadsamaha98@gmail.com>
2021-01-14 19:31:47 +0000

    optimization

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """function"""
+    rms_op = tf.train.RMSPropOptimizer(alpha, decay=beta, epsilon=epsilon)
+    return rms_op.minimize(loss)
