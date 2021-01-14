40dff14c21fadb216ace2e3f3727ca1f145e2024
jadsm98 <jadsamaha98@gmail.com>
2021-01-14 19:55:44 +0000

    optimization

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -6,7 +6,7 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """function"""
     Vd = beta1 * v + (1 - beta1) * grad
     Sv = beta2 * s + (1 - beta2) * grad ** 2
-    Vd_new = Vd / (1 - beta ** t)
-    Sv_new = Sv / (1 - beta ** t)
+    Vd_new = Vd / (1 - beta1 ** t)
+    Sv_new = Sv / (1 - beta2 ** t)
     update = var - alpha * Vd_new / (Sv_new ** 0.5 + epsilon)
     return update, Vd, Sv
