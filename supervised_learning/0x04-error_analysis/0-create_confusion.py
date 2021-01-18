2f84463fdf4ead188ab6bba5b65aad3ce7ef001d
jadsm98 <jadsamaha98@gmail.com>
2021-01-18 16:20:52 +0000

    confusion

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+module
+"""
+
+
+def create_confusion_matrix(labels, logits):
+    """function"""
+    
+    labels = list(labels)
+    logits = list(logits)
+    conf_matrix = [[0 for i in range(len(labels[0]))]
+                   for i in range(len(labels[0]))]
+    for i in range(len(labels)):
+        for j in range(len(labels[0])):
+            if labels[i][j] == 1:
+                for k in range(len(logits[0])):
+                    if logits[i][k] == 1:
+                        conf_matrix[j][k] += 1
+    return np.array(conf_matrix).astype('float64')
---
+++ supervised_learning/0x04-error_analysis/README.md
@@ -0,0 +1,1 @@
+This is my error analysis project
