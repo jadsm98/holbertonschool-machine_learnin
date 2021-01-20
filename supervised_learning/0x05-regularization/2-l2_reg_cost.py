8df9c4a2c6b86fcaed205874a29f0ff639aeb1e0
jadsm98 <jadsamaha98@gmail.com>
2021-01-20 20:02:20 +0000

    L2 cost

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -7,4 +7,9 @@ import tensorflow as tf
 
 def l2_reg_cost(cost):
     """function"""
-    return cost + tf.contrib.layers.l2_regularizer(0.1)
+    regularizer = tf.contrib.layers.l2_regularizer(scale=0.1)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    weights = tf.get_variable(initializer=init, regularizer=regularizer)
+    reg_variables = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    reg_term = tf.contrib.layers.apply_regularization(regularizer, reg_variables)
+    return cost + reg_term
