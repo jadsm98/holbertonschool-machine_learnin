041d2af140eb4b93dd27ae9986119b493943f7e5
jadsm98 <jadsamaha98@gmail.com>
2021-01-21 20:36:17 +0000

    l2 cost

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -8,4 +8,4 @@ import tensorflow as tf
 def l2_reg_cost(cost):
     """function"""
     reg_losses = tf.losses.get_regularization_losses()
-    return tf.math.add(cost, reg_losses)
+    return tf.math.add(cost, tf.reduce_sum(reg_losses))
