4401205ed489d7175237c5ca4696df6a81082569
jadsm98 <jadsamaha98@gmail.com>
2021-01-21 16:55:07 +0000

    L2 cost

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -7,14 +7,15 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """method"""
+    copied = weights.copy()
     m = Y.shape[1]
     for i in range(L, 0, -1):
         if i == L:
             dz = cache['A{}'.format(L)] - Y
         else:
             dz = np.multiply(np.matmul(
-                weights['W{}'.format(i + 1)].T, dz),
-               -np.square(cache['A{}'.format(i)]) + 1)
+                copied['W{}'.format(i + 1)].T, dz),
+                -np.square(cache['A{}'.format(i)]) + 1)
         dw = np.matmul(dz, cache['A{}'.format(i - 1)].T) / m
         db = np.sum(dz, axis=1, keepdims=True) / m
         reg = dw + (lambth/m) * weights['W{}'.format(i)]
