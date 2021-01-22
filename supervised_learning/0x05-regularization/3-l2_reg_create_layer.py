ef2a8c6555e665ab3c1366bb1dab777302dfd8f5
jadsm98 <jadsamaha98@gmail.com>
2021-01-22 14:41:44 +0000

    l2

---
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """function"""
+    weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    l2_reg = tf.contrib.layers.l2_regularizer(lambtha)
+    weights_reg = l2_reg(weights)
+    layer = tf.layers.Dense(units=n, activation=activation)
+    return layer(weights_reg)
