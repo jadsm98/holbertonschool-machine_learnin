7e37c138d4eeb1788c6bb7a3b1e6e9427bdb73b2
jadsm98 <jadsamaha98@gmail.com>
2021-01-26 14:18:10 +0000

    keras

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -2,14 +2,9 @@
 """module"""
 
 
-import numpy as np
-
+import tensorflow.keras as K
 
 def one_hot(labels, classes=None):
     """function"""
-    if type(labels) is not np.ndarray or len(labels) == 0:
-        return None
-    if classes is None:
-        classes = np.max(labels) + 1
-    A = np.eye(classes)[labels]
-    return A
+    return K.utils.to_categorical(labels,
+                                  num_classes=classes)
