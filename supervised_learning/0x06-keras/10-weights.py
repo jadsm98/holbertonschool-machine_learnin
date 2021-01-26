a5745ff0da7888e4637e5a0f9fded2fb2d0d7177
jadsm98 <jadsamaha98@gmail.com>
2021-01-26 17:03:42 +0000

    keras

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """function"""
+    network.save_weights(filename + '.' + save_format)
+    return None
+
+
+def load_weights(network, filename):
+    """function"""
+    network.load_weights(filename + '.' + save_format)
+    return None
