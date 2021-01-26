4fab9fbf96bd3d01afc41c49b28b4c7985a3e152
jadsm98 <jadsamaha98@gmail.com>
2021-01-26 15:09:12 +0000

    keras

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -9,8 +9,10 @@ def train_model(network, data, labels, batch_size,
                 epochs, validation_data=None,
                 verbose=True, shuffle=False):
     """function"""
+    if not type(validation_data) is tuple:
+        validation_data = None
     history = network.fit(data, labels, batch_size=batch_size,
                           epochs=epochs, verbose=verbose,
                           shuffle=shuffle,
                           validation_data=validation_data)
-    return K.callback.History()
+    return history
