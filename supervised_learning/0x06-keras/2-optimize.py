68602f4d9c257cf76d6fec410ddef8f9b4ae517b
jadsm98 <jadsamaha98@gmail.com>
2021-01-26 11:15:21 +0000

    keras

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -8,5 +8,5 @@ import tensorflow.keras as K
 def optimize_model(network, alpha, beta1, beta2):
     """function"""
     opt = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
-    network.compile(loss='categorical_crossentropy', optimizer= opt,
+    network.compile(loss='categorical_crossentropy', optimizer=opt,
                     metrics=["accuracy"])
