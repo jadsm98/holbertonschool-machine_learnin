50e2a20261c40f739b441f55120a8e132e189dda
jadsm98 <jadsamaha98@gmail.com>
2021-01-26 11:12:28 +0000

    keras

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -11,11 +11,8 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     l2 = K.regularizers.l2(lambtha)
     x = K.layers.Dense(layers[0], activation=activations[0],
                        kernel_regularizer=l2)(inputs)
-    drop = K.layers.Dropout(1 - keep_prob)(x)
-    for layer in range(1, len(layers) - 1):
+    for layer in range(1, len(layers)):
+        drop = K.layers.Dropout(1 - keep_prob)(x)
         x =  K.layers.Dense(layers[layer], activation=activations[layer],
                             kernel_regularizer=l2)(drop)
-        drop = K.layers.Dropout(1 - keep_prob)(x)
-    output = K.layers.Dense(layers[-1], activation=activations[-1],
-                            kernel_regularizer=l2)(drop)
-    return K.Model(inputs=inputs, outputs=output)
+    return K.Model(inputs=inputs, outputs=x)
