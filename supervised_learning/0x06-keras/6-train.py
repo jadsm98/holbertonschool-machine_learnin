21fe58537a0a90e14a33723f58e2dd1249b8e92e
jadsm98 <jadsamaha98@gmail.com>
2021-01-26 13:53:33 +0000

    keras

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -10,12 +10,13 @@ def train_model(network, data, labels, batch_size,
                 early_stopping=False, patience=0,
                 verbose=True, shuffle=False):
     """function"""
-    if early_stopping and not validation_data is None:
+    if early_stopping and validation_data is not None:
         early_stop = K.callbacks.EarlyStopping(monitor="val_loss",
                                                patience=patience)
     else:
         early_stop = None
     history = network.fit(data, labels, batch_size=batch_size,
-                          epochs=epochs, verbose=verbose, callbacks=[early_stop],
-                          shuffle=shuffle, validation_data=validation_data)
+                          epochs=epochs, verbose=verbose,
+                          callbacks=[early_stop], shuffle=shuffle,
+                          validation_data=validation_data)
     return history
