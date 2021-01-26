2f379fafce45289a97dd800734f0c618a5e82e86
jadsm98 <jadsamaha98@gmail.com>
2021-01-26 13:16:00 +0000

    keras

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -7,9 +7,9 @@ import numpy as np
 
 def one_hot(labels, classes=None):
     """function"""
-    target = labels.reshape(-1)
+    if type(labels) is not np.ndarray or len(labels) == 0:
+        return None
     if classes is None:
-        one_hot = np.eye(np.max(labels)+1)[target]
-    else:
-        one_hot = np.eye(classes)[target]
-    return one_hot
+        classes = np.max(labels) + 1
+    A = np.eye(classes)[labels]
+    return A
