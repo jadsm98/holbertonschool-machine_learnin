3d9a73cc4cbaae570246c0919afc64654435ec4f
jadsm98 <jadsamaha98@gmail.com>
2021-01-26 15:06:42 +0000

    keras

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -11,5 +11,6 @@ def train_model(network, data, labels, batch_size,
     """function"""
     history = network.fit(data, labels, batch_size=batch_size,
                           epochs=epochs, verbose=verbose,
-                          shuffle=shuffle, validation_data=validation_data)
+                          shuffle=shuffle,
+                          validation_data=validation_data)
     return history
