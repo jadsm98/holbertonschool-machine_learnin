97b44e339b96159cfa46ac8a9f57ea35f4fb6c9e
jadsm98 <jadsamaha98@gmail.com>
2021-01-26 11:28:47 +0000

    keras

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -7,11 +7,6 @@ import numpy as np
 
 def one_hot(labels, classes=None):
     """function"""
-    if classes is None:
-        maximum = np.max(labels)
-        one_hot = np.zeros((labels.shape[0], maximum + 1))
-        one_hot[np.arange(labels.shape[0]), labels] = 1
-    else:
-        one_hot = np.zeros((labels.shape[0], classes))
-        one_hot[np.arange(labels.shape[0]), labels] = 1
+    one_hot = np.zeros((labels.shape[0], classes))
+    one_hot[np.arange(labels.shape[0]), labels] = 1
     return one_hot
