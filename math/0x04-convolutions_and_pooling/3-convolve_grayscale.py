9e02ebf2a17715d8002e8441d2bb12f20983f512
jadsm98 <jadsamaha98@gmail.com>
2021-01-28 16:59:12 +0000

    convolution

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -11,25 +11,21 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     m, in_h, in_w, = images.shape
     k_h, k_w = kernel.shape
     if padding == 'valid':
-        out_h = int(ceil(float(in_h - k_h + 1) / float(stride[0])))
-        out_w = int(ceil(float(in_w - k_w + 1) / float(stride[1])))
+        out_h = (in_h - k_h + 1) // stride[0]
+        out_w = (in_w - k_w + 1) // stride[1]
     elif padding == 'same':
-        out_h = int(ceil(float(in_h) / float(stride[0])))
-        out_w = int(ceil(float(in_w) / float(stride[1])))
-        p_h = max((out_h - 1) * stride[0] + k_h - in_h, 0)
-        p_w = max((out_w - 1) * stride[1] + k_w - in_w, 0)
-        p_t = p_h // 2
-        p_b = p_h - p_t
-        p_l = p_w // 2
-        p_r = p_w - p_l
-        image_padded = np.zeros((m, in_h + p_h, in_w + p_w))
-        image_padded[:, p_t: - p_b, p_l: - p_r] = images
+        out_h = int(ceil(in_h / stride[0]))
+        out_w = int(ceil(in_w / stride[1]))
+        p_h = ((out_h - 1) * stride[0] + k_h - in_h) // 2
+        p_w = ((out_w - 1) * stride[1] + k_w - in_w) // 2
+        image_padded = np.pad(images, [(0, 0), (p_h, p_h),
+                                       (p_w, p_w)])
     else:
         p_h, p_w = padding
-        out_w = int(ceil(float(in_w - k_w + 2 * p_w + 1) / float(stride[1])))
-        out_h = int(ceil(float(in_h - k_h + 2 * p_h + 1) / float(stride[0])))
-        image_padded = np.zeros((m, in_h + 2 * p_h, in_w + 2 * p_w))
-        image_padded[:, p_h: - p_h, p_w: - p_w] = images
+        out_w = int(ceil((in_w - k_w + 2 * p_w + 1) / stride[1]))
+        out_h = int(ceil((in_h - k_h + 2 * p_h + 1) / stride[0]))
+        image_padded = np.pad(images, [(0, 0), (p_h, p_h),
+                                       (p_w, p_w)])
     output = np.zeros((m, out_h, out_w))
     for i in range(out_h):
         for j in range(out_w):
