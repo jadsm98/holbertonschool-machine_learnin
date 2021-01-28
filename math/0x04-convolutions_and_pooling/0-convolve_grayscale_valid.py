1c3a1784cac9f370fb61411696150fb5faed56f6
jadsm98 <jadsamaha98@gmail.com>
2021-01-28 12:14:17 +0000

    convolution

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -7,15 +7,16 @@ import numpy as np
 
 def convolve_grayscale_valid(images, kernel):
     """function"""
+    m = images.shape[0]
     in_h = images.shape[1]
     in_w = images.shape[2]
     k_h = kernel.shape[0]
     k_w = kernel.shape[1]
-    out_h = int(ceil(float(in_h - k_h + 1)))
-    out_w = int(ceil(float(in_w - k_w + 1)))
-    output = np.zeros((images.shape[0], out_h, out_w))
-    for x in range(out_w):
-        for y in range(out_h):
+    out_h = in_h - k_h + 1
+    out_w = in_w - k_w + 1
+    output = np.zeros((m, out_h, out_w))
+    for y in range(out_h):
+        for x in range(out_w):
             im_slice = images[:, y:y + k_h, x:x + k_w]
             output[:, y, x] = np.sum(kernel * im_slice, axis=(1, 2))
     return output
