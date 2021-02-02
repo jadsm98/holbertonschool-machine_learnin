a731638d5bac5c40ce8835c2f2eefcad275605cc
jadsm98 <jadsamaha98@gmail.com>
2021-02-02 23:54:19 +0000

    CNN

---
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import numpy as np
+
+
+def conv_forward(A_prev, W, b, activation, padding="same",
+                 stride=(1, 1)):
+    """function"""
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw = W.shape[0], W.shape[1]
+    c_new = W.shape[3]
+    sh, sw = stride
+    if padding == 'valid':
+        h_new = int(np.floor(((h_prev - kh)/sh) + 1))
+        w_new = int(np.floor(((w_prev - kw)/sw) + 1))
+    else:
+        ph = int(np.ceil(((h_prev - 1)*sh + kh - h_prev + 1)/2))
+        pw = int(np.ceil(((w_prev - 1)*sw + kw - w_prev + 1)/2))
+        h_new = int(np.floor(((h_prev + 2*ph - kh + 1)/sh)))
+        w_new = int(np.floor(((w_prev + 2*pw - kw + 1)/sw)))
+        A_padded = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
+    output = np.zeros((m, h_new, w_new, c_new))
+    for k in range(c_new):
+        for i in range(h_new):
+            for j in range(w_new):
+                if padding == 'valid':
+                    slice = A_prev[:, i*sh: i*sh + kh, j*sw: j*sw + kw, :]
+                else:
+                    slice = A_padded[:, i*sh: i*sh + kh, j*sw: j*sw + kw, :]
+                output[:, i, j, k] = np.sum(activation(slice*W[:, :, :, k] + b), axis=(1, 2, 3))
+    return output
