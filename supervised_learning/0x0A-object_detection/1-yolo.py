786b0460837234a31079eb52912c8ec0f6443c3a
jadsm98 <jadsamaha98@gmail.com>
2021-02-27 15:10:02 +0000

    object detection

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -1,62 +1,54 @@
 #!/usr/bin/env python3
-"""class for model Yolo"""
+"""module"""
+
+
 import tensorflow.keras as K
 import numpy as np
 
 
-class Yolo():
-    """class Yolo that uses Yolov3 algorithm"""
+class Yolo:
+    """class"""
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """constructor for a Yolo class"""
+        """initializer"""
         self.model = K.models.load_model(model_path)
-        with open(classes_path, 'r') as classes:
-            lines = [line.split("\n")[0] for line in classes.readlines()]
-        self.class_names = lines
+        with open(classes_path, 'r') as file:
+            self.class_names = file.read().splitlines()
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
-
-    def sigmoid(self, z):
-        """performs sigmoid mapping"""
-        return (1 / (1 + np.exp(-z)))
+    
+    def sig(self, X):
+        """function"""
+        return 1/(1 + np.exp(-X))
 
     def process_outputs(self, outputs, image_size):
-        """Returns a tuple of (boxes, box_confidences,
-           box_class_probs)"""
+        """method"""
+        im_h = image_size[0]
+        im_w = image_size[1]
         boxes = []
-        confidences = []
-        class_proba = []
-        img_H = image_size[0]
-        img_W = image_size[1]
-        for output in outputs:
-            boxes.append(output[..., 0:4])
-            confidences.append(self.sigmoid(output[..., 4, np.newaxis]))
-            class_proba.append(self.sigmoid(output[..., 5:]))
-        for i, box in enumerate(boxes):
-            g_h, g_w, achors_box, _ = box.shape
-            coordidate = np.zeros((g_h, g_w, achors_box))
-            idx_y = np.arange(g_h)
-            idx_y = idx_y.reshape(g_h, 1, 1)
-            idx_x = np.arange(g_w)
-            idx_x = idx_x.reshape(1, g_w, 1)
-            C_x = coordidate + idx_x
-            C_y = coordidate + idx_y
-            centerX = box[..., 0]
-            centerY = box[..., 1]
-            width = box[..., 2]
-            height = box[..., 3]
-            bx = (self.sigmoid(centerX) + C_x) / g_w
-            by = (self.sigmoid(centerY) + C_y) / g_h
+        box_confidence = []
+        box_class_prob = []
+        for i in range(len(outputs)):
+            gh, gw, na, c = outputs[i].shape
+            boxes.append(np.zeros((gh, gw, na, 4)))
+            box_confidence.append(np.zeros((gh, gw, na, 1)))
+            box_class_prob.append(np.zeros((gh, gw, na, c - 5)))
+
             pw = self.anchors[i, :, 0]
             ph = self.anchors[i, :, 1]
-            bw = (np.exp(width) * pw) / self.model.input.shape[1].value
-            bh = (np.exp(height) * ph) / self.model.input.shape[2].value
-            x1 = bx - bw / 2
-            y1 = by - bh / 2
-            x2 = x1 + bw
-            y2 = y1 + bh
-            box[..., 0] = x1 * img_W
-            box[..., 1] = y1 * img_H
-            box[..., 2] = x2 * img_W
-            box[..., 3] = y2 * img_H
-        return boxes, confidences, class_proba
+            tx, ty = outputs[i][..., 0], outputs[i][..., 1]
+            tw, th = outputs[i][..., 2], outputs[i][..., 3]
+            cx, cy = np.arange(gw).reshape(1, gw, 1),\
+                np.arange(gh).reshape(gh, 1, 1)
+            bx, by = (self.sig(tx) + cx)/gw, (self.sig(ty) + cy)/gh
+            bw, bh = pw*np.exp(tw)/self.model.input.shape[1].value,\
+                ph*np.exp(th)/self.model.input.shape[2].value
+            x1, y1, x2, y2 = bx - bw/2, by - bh/2, bx + bw/2, by + bh/2
+            boxes[i][..., 0] = x1*im_w
+            boxes[i][..., 1] = y1*im_h
+            boxes[i][..., 2] = x2*im_w
+            boxes[i][..., 3] = y2*im_h
+            box_class_prob[i] = self.sig(outputs[i][..., 5:])
+            box_confidence[i] = self.sig(outputs[i][..., 4])
+        return boxes, box_confidence, box_class_prob
