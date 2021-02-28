f4660da57d23aa1ffa72de8aaf0ed56a181b638e
jadsm98 <jadsamaha98@gmail.com>
2021-02-28 18:58:04 +0000

    object detection

--- supervised_learning/0x0A-object_detection/3-yolo.py
+++ supervised_learning/0x0A-object_detection/3-yolo.py
@@ -4,13 +4,6 @@
 
 import tensorflow.keras as K
 import numpy as np
-import cv2
-import glob
-
-
-def sig(x):
-    """sigmoid"""
-    return 1/(1 + np.exp(-x))
 
 
 class Yolo:
@@ -25,6 +18,10 @@ class Yolo:
         self.nms_t = nms_t
         self.anchors = anchors
 
+    def sig(self, x):
+        """sigmoid"""
+        return 1/(1 + np.exp(-x))
+        
     def process_outputs(self, outputs, image_size):
         """method"""
         im_h = image_size[0]
@@ -37,65 +34,74 @@ class Yolo:
             boxes.append(np.zeros((gh, gw, na, 4)))
             box_confidence.append(np.zeros((gh, gw, na, 1)))
             box_class_prob.append(np.zeros((gh, gw, na, c - 5)))
-            for h in range(gh):
-                for w in range(gw):
-                    for a in range(na):
-                        pw = self.anchors[i, a, 0]
-                        ph = self.anchors[i, a, 1]
-                        tx, ty, tw, th = outputs[i][h, w, a, :4]
-                        cx, cy = w, h
-                        bx, by = (sig(tx) + cx)/gw, (sig(ty) + cy)/gh
-                        bw, bh = pw*np.exp(tw)/self.model.input.shape[1],\
-                            ph*np.exp(th)/self.model.input.shape[2]
-                        x1, y1, x2, y2 = bx - bw/2, by - bh/2, bx + bw/2, by + bh/2
-                        boxes[i][h, w, a, :] = x1*im_w, y1*im_h, x2*im_w, y2*im_h
-                        box_class_prob[i][h, w, a, :] = sig(outputs[i][h, w, a, 5:])
-                        box_confidence[i][h, w, a, :] = sig(outputs[i][h, w, a, 4])
+            pw = self.anchors[i, :, 0]
+            ph = self.anchors[i, :, 1]
+            tx, ty = outputs[i][..., 0], outputs[i][..., 1]
+            tw, th = outputs[i][..., 2], outputs[i][..., 3]
+            cx, cy = np.arange(gw).reshape(1, gw, 1),\
+                     np.arange(gh).reshape(gh, 1, 1)
+            bx, by = (self.sig(tx) + cx)/gw, (self.sig(ty) + cy)/gh
+            bw, bh = pw*np.exp(tw)/self.model.input.shape[1].value,\
+                     ph*np.exp(th)/self.model.input.shape[2].value
+            x1, y1, x2, y2 = bx - bw/2, by - bh/2, bx + bw/2, by + bh/2
+            boxes[i][..., 0] = x1*im_w
+            boxes[i][..., 1] = y1*im_h
+            boxes[i][..., 2] = x2*im_w
+            boxes[i][..., 3] = y2*im_h
+            box_class_prob[i] = self.sig(outputs[i][..., 5:])
+            box_confidence[i] = self.sig(outputs[i][..., 4, np.newaxis])
         return boxes, box_confidence, box_class_prob
 
     def filter_boxes(self, boxes, box_confidences, box_class_probs):
         """method"""
+        score = []
         box = []
-        classes = []
-        scores =[]
         for i in range(len(boxes)):
-            boxScore = box_confidences[i] * box_class_probs[i]
-            boxClass = np.argmax(boxScore, axis=-1)
-            boxScore = np.max(boxScore, axis=-1)
-            box.append(boxes[i][boxScore >= self.class_t])
-            scores.append(boxScore[boxScore >= self.class_t])
-            classes.append(boxClass[boxScore >= self.class_t])
-        box = np.concatenate(box, axis=0)
-        classes = np.concatenate(classes, axis=None)
-        scores = np.concatenate(scores, axis=None)
-        return box, classes, scores
+            box.append(boxes[i].reshape(-1, 4))
+            score.append(box_confidences[i] * box_class_probs[i])
+        boxClass = [np.argmax(i, axis=-1).reshape(-1) for i in score]
+        boxScore = [np.max(i, axis=-1).reshape(-1) for i in score]
+        box = np.concatenate(box)
+        boxClass = np.concatenate(boxClass)
+        boxScore = np.concatenate(boxScore)
+        mask = np.where(boxScore >= self.class_t)
+        filtered_boxes = box[mask]
+        box_classes = boxClass[mask]
+        box_scores = boxScore[mask]
+        return filtered_boxes, box_classes, box_scores
 
     def non_max_suppression(self, filtered_boxes, box_classes, box_scores):
         """method"""
-        pick = []
-        x1, y1, x2, y2 = filtered_boxes[:, 0], filtered_boxes[:, 1],\
-            filtered_boxes[:, 2], filtered_boxes[:, 3]
-        area = (x2 - x1)*(y2 - y1)
-        idxs = np.argsort(y2)
-        while len(idxs) > 0:
-            last = len(idxs) - 1
-            i = idxs[last]
-            pick.append(i)
-            suppress = [last]
-            for pos in range(0, last):
-                j = idxs[pos]
-                xx1 = max(x1[i], x1[j])
-                yy1 = max(y1[i], y2[j])
-                xx2 = min(x2[i], x2[j])
-                yy2 = min(y2[i], y2[j])
+        filtered_box = []
+        filtered_classes = []
+        filtered_scores = []
+        for uniq in np.unique(box_classes):
+            cl = np.where(uniq == box_classes)
+            box = filtered_boxes[cl]
+            classes = box_classes[cl]
+            scores = box_scores[cl]
 
-                w = max(0, xx2 - xx1)
-                h = max(0, yy2 - yy1)
-                overlap = float(w*h)/area[j]
-                if overlap > self.nms_t:
-                    suppress.append(pos)
-            idxs = np.delete(idxs, suppress)
-        box_pred = filtered_boxes[pick]
-        class_pred = box_classes[pick]
-        scores_pred = box_scores[pick]
-        return box_pred, class_pred, scores_pred
+            pick = []
+            x1, y1, x2, y2 = box[:, 0], box[:, 1],\
+                box[:, 2], box[:, 3]
+            area = (x2 - x1 + 1)*(y2 - y1 + 1)
+            idxs = scores.argsort()[::-1]
+            while idxs.size > 0:
+                i = idxs[0]
+                pick.append(i)
+                xx1 = np.maximum(x1[i], x1[idxs[1:]])
+                yy1 = np.maximum(y1[i], y2[idxs[1:]])
+                xx2 = np.minimum(x2[i], x2[idxs[1:]])
+                yy2 = np.minimum(y2[i], y2[idxs[1:]])
+
+                w = np.maximum(0, xx2 - xx1 + 1)
+                h = np.maximum(0, yy2 - yy1 + 1)
+                overlap = (w*h)/(area[i] + area[idxs[1:]] - w*h)
+                idxs = idxs[np.where(overlap <= self.nms_t)[0] + 1]
+            filtered_box.append(box[pick])
+            filtered_classes.append(classes[pick])
+            filtered_scores.append(scores[pick])
+        filtered_box = np.concatenate(filtered_boxes)
+        filtered_classes = np.concatenate(filtered_classes)
+        filtered_scores = np.concatenate(filtered_scores)
+        return filtered_box, filtered_classes, filtered_scores
