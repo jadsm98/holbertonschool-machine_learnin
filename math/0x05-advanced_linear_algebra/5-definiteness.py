21fb3bd45bd6eb03830e2c310c82bf5cf21faa45
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-18 20:18:01 +0200

    definiteness

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def definiteness(matrix):
     """function"""
-    if not isinstance(marix, np.ndarray):
+    if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
     if not any(len(i) == len(matrix) for i in matrix) or len(matrix) == 0:
         return None
