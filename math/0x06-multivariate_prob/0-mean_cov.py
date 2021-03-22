80c6e21567959f3226d24d1829ef9c2b8a075eb0
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-22 19:10:52 +0200

    mean/cov

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def mean_cov(X):
     """function"""
-    if not isinstance(X, np.ndarray) and len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
