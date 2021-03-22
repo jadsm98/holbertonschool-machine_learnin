af3f909de294dde090423d1d7086f780cdd49812
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-22 20:19:15 +0200

    multinormal

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -12,10 +12,10 @@ class MultiNormal:
         """initializer"""
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        if data.shape[0] < 2:
+        if data.shape[1] < 2:
             raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1).reshape((data.shape[0], 1))
-        data = data.T
-        mean = np.mean(data, axis=0)
-        var = data - mean
-        self.cov = np.matmul(var.T, var)/(data.shape[0] - 1)
+        data_t = data.T
+        mean = np.mean(data_t, axis=0)
+        var = data_t - mean
+        self.cov = np.matmul(var.T, var)/(data_t.shape[0] - 1)
