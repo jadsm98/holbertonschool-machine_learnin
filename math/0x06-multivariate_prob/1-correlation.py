0c7fe6e2190fc5d0ba3939e2c01bdedd5ff00cf7
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-22 19:48:31 +0200

    correlation

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import numpy as np
+
+
+def correlation(C):
+    """function"""
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+    if len(X.shape) != 2 and X.shape[0] != X.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+    diag = np.sqrt(np.diag(C))
+    corr = np.zeros(C.shape)
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            corr[i][j] = C[i][j]/(diag[i]*diag[j])
+    return corr
