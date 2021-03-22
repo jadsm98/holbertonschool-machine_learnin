efdb6d72dab891c7a87cd5cd864266c0f455fc63
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-22 14:49:25 +0200

    mean/cov

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import numpy as np
+
+
+def mean_cov(X):
+    """function"""
+    if not isinstance(X, np.ndarray) and len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+    mean = np.sum(X, axis=0)/X.shape[0]
+    var = X - mean
+    cov = np.matmul(var.T, var)/(X.shape[0] - 1)
+    return mean, cov
