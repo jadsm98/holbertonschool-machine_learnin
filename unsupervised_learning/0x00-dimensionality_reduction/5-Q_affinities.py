749a91ea678467b3d07743f91cc50048adac1796
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-24 23:21:35 +0200

    Q_affinities

---
+++ unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import numpy as np
+
+
+def Q_affinities(Y):
+    """function"""
+    n, ndim = Y.shape
+    D = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            D[i][j] = np.power(np.linalg.norm(X[i,:] - X[j,:]), 2)
+    num = 1/(1 + D)
+    denum = np.sum(num)
+    Q = num/denum
+    return Q, num
