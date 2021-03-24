3c94d49ef8f119e8f297f0d5959bc582b3ab150c
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-24 22:51:52 +0200

    P_init

---
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import numpy as np
+
+
+def P_init(X, perplexity):
+    """function"""
+    n, d = X.shape
+    D = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            D[i][j] = np.linalg.norm(X[i,:] - X[j,:])
+    P = np.zeros((n, n))
+    betas = np.ones(n).reshape((n, 1))
+    H = np.log2(perplexity)
+    return (D, P, betas, H)
