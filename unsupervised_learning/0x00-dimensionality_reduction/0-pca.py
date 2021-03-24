91dd7cc09395f04204cfffde6f632bb4ad90581e
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-24 20:42:41 +0200

    pca

---
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module"""
+
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """function"""
+    U, sig, V = np.linalg.svd(X)
+    cum_sum = np.cumsum(sig)
+    for i, elem in enumerate(cum_sum):
+        if elem/cum_sum[-1] >= var:
+            r = i
+            break
+    W = V.T[:, :r]
+    return W
