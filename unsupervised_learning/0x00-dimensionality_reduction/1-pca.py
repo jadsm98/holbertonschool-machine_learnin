a3aba7de19469f2232ffd58c7281e497d4d10465
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-24 21:17:06 +0200

    pca

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def pca(X, ndim):
     """function"""
-    mean = np.mean(X, axis=0)
+    mean = X - np.mean(X, axis=0)
     _, _, V = np.linalg.svd(mean)
     W = V.T[:, :ndim]
     return np.matmul(mean, W)
