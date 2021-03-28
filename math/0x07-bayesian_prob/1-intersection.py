16171f02b13c8dc5ea05aacb70168d6366cdd3c1
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-28 18:12:42 +0300

    intersection

---
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""module"""
+
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """function"""
+
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        err = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(err)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+    n_fac = np.math.factorial(n)
+    x_fac = np.math.factorial(x)
+    n_x_fac = np.math.factorial(n - x)
+    comb = n_fac/(x_fac*n_x_fac)
+    likelihood = comb * np.power(P, x) * np.power(1 - P, n - x)
+    return likelihood * Pr
