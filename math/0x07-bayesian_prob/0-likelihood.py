13f7a41c4921dd4e093a5cda03eafec7add1d874
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-03-28 17:16:58 +0300

    likelihood

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -8,16 +8,16 @@ import numpy as np
 def likelihood(x, n, P):
     """function"""
 
-    if n <= 0 or not type(n) is int:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not type(x) is int or x < 0:
+    if type(x) is not int or x < 0:
         raise ValueError("x must be an integer that is\
                           greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if not np.any(P >= 0) or not np.any(P <= 1):
+    if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range [0, 1]")
     n_fac = np.math.factorial(n)
     x_fac = np.math.factorial(x)
