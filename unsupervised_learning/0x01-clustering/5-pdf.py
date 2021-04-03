9144be40981ee70db470788d7fd67d0dbef865ff
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-03 17:44:31 +0300

    pdf

---
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import numpy as np
+
+
+def pdf(X, m, S):
+    """function"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
+        return None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 2:
+        return None
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
+        return None
+    if S.shape[0] != S.shape[1]:
+        return None
+    inv = np.linalg.inv(S)
+    det = np.linalg.det(S)
+    denum = np.sqrt(np.power((2 * np.pi), S.shape[0]) * det)
+    y = np.matmul((X - m).T, inv)
+    pdf = (1 / denum) * np.exp(-1 * np.matmul(y, (X - m)) / 2)
+    pdf = pdf.reshape(-1)[0]
+    return np.where(pdf < 1e-300, 1e-300. pdf)
