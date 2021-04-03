a34eecf9b013f2f92738eb76223a42c25315e197
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-03 12:56:56 +0300

    initialize

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -8,6 +8,8 @@ def initialize(X, k):
     """function"""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
+    if not type(k) is int or k <= 0:
+        return None
     try:
         min = np.amin(X, axis=0)
         max = np.amax(X, axis=0)
