da7088658ab357126efceb60f1474c104aa60571
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-03 17:32:31 +0300

    initialize

---
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -0,0 +1,19 @@
+  
+#!/usr/bin/env python3
+"""module"""
+
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """function"""
+     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if not isinstance(k, int) or k <= 0 or X.shape[0] < k:
+        return None, None, None
+    _, d = X.shape
+    pi = np.full((k,), 1/k)
+    m, _ = kmeans(X, k)
+    S = np.tile(np.identity(d), (k, 1, 1))
+    return pi, m, S
