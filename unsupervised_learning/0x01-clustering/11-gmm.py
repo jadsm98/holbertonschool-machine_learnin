3859de6e7055ddb72bd51881223b3fea409517a7
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-04 02:30:15 +0300

    gmm

--- unsupervised_learning/0x01-clustering/11-gmm.py
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -5,7 +5,7 @@ import sklearn.mixture
 
 
 def gmm(X, k):
-  """function"""
-  gm = sklearn.mixture.GaussianMixture(n_components=k).fit(X)
-  return gm.weights_, gm.means_, gm.covariances_,
-         gm.predict(X), gm.bic(X)
+    """function"""
+    gm = sklearn.mixture.GaussianMixture(n_components=k).fit(X)
+    return gm.weights_, gm.means_, gm.covariances_,
+           gm.predict(X), gm.bic(X)
