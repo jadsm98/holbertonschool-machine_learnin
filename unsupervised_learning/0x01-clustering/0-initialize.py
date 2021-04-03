dc669e620dd17b1935e623b09dc59f0d447dab0d
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-03 12:51:09 +0300

    initialize

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -9,7 +9,8 @@ def initialize(X, k):
     try:
         min = np.amin(X, axis=0)
         max = np.amax(X, axis=0)
-        clusters = np.random.uniform(low=min, high=max, size=(k, X.shape[1]))
+        clusters = np.random.uniform(low=min, high=max,
+                                     size=(k, X.shape[1]))
         return clusters
-    except:
+    except Exception:
         return None
