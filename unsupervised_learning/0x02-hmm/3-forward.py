45d2ed9c3f18558e9e277b06b9b92a48c888fd79
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-08 19:37:47 +0300

    forward

--- unsupervised_learning/0x02-hmm/3-forward.py
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -8,25 +8,24 @@ import numpy as np
 def forward(Observation, Emission, Transition, Initial):
     """function"""
     if not isinstance(Observations, np.ndarray) or \
-        len(Observations.shape) != 1:
+            len(Observations.shape) != 1:
         return None, None
     if not isinstance(Emission, np.ndarray) or \
-        len(Emission.shape) != 2:
+            len(Emission.shape) != 2:
         return None, None
     if not isinstance(Transition, np.ndarray) or \
-        len(Transition.shape) != 2 or \
-        Transition.shape[0] != Transition.shape[1]:
+            len(Transition.shape) != 2 or \
+            Transition.shape[0] != Transition.shape[1]:
         return None, None
     if not isinstance(Initial, np.ndarray) or \
-        len(Initial.shape) != 2 or Initial.shape[1] != 1:
+            len(Initial.shape) != 2 or Initial.shape[1] != 1:
         return None, None
-        
     alpha = np.zeros((Emission.shape[0], Observations.shape[0]))
     for s in range(Initial.shape[0]):
         alpha[s, 0] = Initial[s] * Emission[s, Observations[0]]
     for t in range(1, Observation.shape[0]):
         for j in range(Emission.shape[0]):
             alpha[j, t] = alpha[:, t - 1].dot(Transition[:, j]) * \
-            Emission[j, Observation[t]]
+                            Emission[j, Observation[t]]
     likelihood = np.sum(alpha[:, -1])
     return likelihood, alpha
