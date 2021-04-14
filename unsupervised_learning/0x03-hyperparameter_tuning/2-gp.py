eddf4ba6d6a4034bc6bf67b6c5e112dc9314f283
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-14 21:00:40 +0200

    gp

--- unsupervised_learning/0x03-hyperparameter_tuning/2-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/2-gp.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """
 Gaussian Process module
@@ -22,6 +21,27 @@ class GaussianProcess:
         self.l = l
         self.sigma_f = sigma_f
         self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the Kernel
+        """
+        X = np.zeros((X1.shape[0], X2.shape[0]))
+        for m in range(X1.shape[0]):
+            for n in range(X2.shape[0]):
+                X[m, n] = (X1[m] - X2[n])**2
+
+        k = self.sigma_f**2 * np.exp(-X/(2*self.l**2))
+        return k
+
+    def predict(self, X_s):
+        """method"""
+        K_star = self.kernel(self.X, X_s)
+        K_star_star = self.kernel(X_s, X_s)
+        K_inv = np.linalg.inv(self.K)
+        mean = np.matmul(np.matmul(K_star.T, K_inv), self.Y)
+        sigma = K_star_star - np.matmul(np.matmul(K_star.T, K_inv), K_star)
+        return mean.reshape(-1), np.diag(sigma)
 
     def update(self, X_new, Y_new):
         """method"""
