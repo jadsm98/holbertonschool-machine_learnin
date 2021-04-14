8e06e14c02dc3bf6897ee2762f67c35215151330
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-14 22:36:47 +0200

    bayes opt

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,18 @@
+#1/usr/bin/env python3
+"""module"""
+
+GP = __import__('2-gp').GaussianProcess
+import numpy as np
+
+
+class BayesianOptimization:
+    """class"""
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """Initializer"""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape((ac_samples, 1))
+        self.xsi = xsi
+        self.minimize = minimize
