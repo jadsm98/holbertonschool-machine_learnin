14598b3343e1d1b884b4ce2af11d57d01617cde4
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-14 11:18:15 +0200

    gp

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import numpy as np
+
+
+class GaussianProcess:
+    """class"""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """constuctor"""
+
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """method"""
+        X = np.zeros((X1.shape[0], X2.shape[0]))
+        for m in range(X1.shape[0]):
+            for n in range(X2.shape[0]):
+                X[m, n] = (X1[m] - X2[n])**2
+
+        k = self.sigma_f**2 * np.exp(-X/(2*self.l**2))
+        return k
