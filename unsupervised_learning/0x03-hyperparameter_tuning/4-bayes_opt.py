1e03076ea3b9245b4d7d773b62911b5213eab2de
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-17 14:13:40 +0200

    BO

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""module"""
+
+GP = __import__('2-gp').GaussianProcess
+import numpy as np
+
+
+class BayesianOptimization:
+    """class"""
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """Initializer"""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape((ac_samples, 1))
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """method"""
+        mue, sig = self.gp.predict(self.X_s)
+        if self.minimize:
+            f_best = np.amin(self.gp.Y)
+            upper = - mue + f_best - self.xsi
+            Z = np.where(sig > 0, upper/sig, 0)
+        else:
+            f_best = np.amax(self.gp.Y)
+            upper = mue - f_best - self.xsi
+            Z = np.where(sig > 0, upper / sig, 0)
+        cdf = norm.cdf(Z)
+        pdf = norm.pdf(Z)
+        eq = upper * cdf + sig * pdf
+        EI = np.where(sig > 0, eq, 0)
+        X_next = self.X_s[np.argmax(EI)]
+        return X_next, EI
