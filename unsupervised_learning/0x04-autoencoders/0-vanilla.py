1bcf4634657bb00167cf945cbd24b964787edf8e
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-22 14:32:56 +0200

    vanilla

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 """module"""
 
+
 import tensorflow.keras as keras
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """function"""
-    l = len(hidden_layers) - 1
-    inp1 = keras.Input((input_dims,))
+    inp1 = keras.Input(shape=(input_dims,))
     x = inp1
     for layer in hidden_layers:
-        x = keras.layers.Dense(layer, activation='relu')(x)
-    x = keras.layers.Dense(latent_dims, activation='relu')(x)
-
+        x = keras.layers.Dense(layer, activation="relu")(x)
+    x = keras.layers.Dense(latent_dims, activation="relu")(x)
     encoder = keras.Model(inputs=inp1, outputs=x)
-    inp2 = keras.Input((latent_dims,))
+    inp2 = keras.Input(shape=(latent_dims,))
     x = inp2
     for layer in hidden_layers[::-1]:
-        x = keras.layers.Dense(layer, activation='relu')(x)
-    x = keras.layers.Dense(input_dims, activation='sigmoid')(x)
-
+        x = keras.layers.Dense(layer, activation="relu")(x)
+    x = keras.layers.Dense(input_dims, activation="sigmoid")(x)
     decoder = keras.Model(inputs=inp2, outputs=x)
     x = encoder(inp1)
     out = decoder(x)
     auto = keras.Model(inputs=inp1, outputs=out)
-
-    auto.compile(optimizer='adam', loss='binary_crossentropy')
+    auto.compile(optimizer="adam", loss="binary_crossentropy")
     return encoder, decoder, auto
