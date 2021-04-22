2e723e1cc1ae4f90fe7509594bc527ea907ddb65
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-22 14:17:09 +0200

    vanilla

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -5,32 +5,19 @@ import tensorflow.keras as keras
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
-    """function"""
-    l = len(hidden_layers) - 1
-    encoder = keras.Sequential()
-    for i, layer in enumerate(hidden_layers):
-        if i == 0:
-            encoder.add(keras.layers.Dense(layer, input_shape=(input_dims,),
-                                           activation='relu'))
-        else:
-            encoder.add(keras.layers.Dense(layer, activation='relu'))
-    encoder.add(keras.layers.Dense(latent_dims, activation='relu'))
-
-    decoder = keras.Sequential()
-    for i in range(l, -1, -1):
-        if i == l:
-            decoder.add(keras.layers.Dense(hidden_layers[i],
-                                           input_shape=(latent_dims,),
-                                           activation='relu'))
-        else:
-            decoder.add(keras.layers.Dense(hidden_layers[i],
-                                           activation='relu'))
-    decoder.add(keras.layers.Dense(input_dims, activation='sigmoid'))
-
-    inp = keras.Input(shape=(input_dims,))
-    x = encoder(inp)
-    out = decoder(x)
-    auto = keras.Model(inputs=inp, outputs=out)
-
-    auto.compile(optimizer='adam', loss='binary_crossentropy')
-    return encoder, decoder, auto
+    """A vanilla autoencoder"""
+    encodein = keras.Input(shape=(input_dims,))
+    latents = encodein
+    for layer in hidden_layers:
+        latents = keras.layers.Dense(layer, activation="relu")(latents)
+    latents = keras.layers.Dense(latent_dims, activation="relu")(latents)
+    encoder = keras.Model(encodein, latents)
+    decodein = keras.Input(shape=(latent_dims,))
+    recons = decodein
+    for layer in hidden_layers[::-1]:
+        recons = keras.layers.Dense(layer, activation="relu")(recons)
+    recons = keras.layers.Dense(input_dims, activation="sigmoid")(recons)
+    decoder = keras.Model(decodein, recons)
+    autoencoder = keras.Model(encodein, decoder(encoder(encodein)))
+    autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
+    return encoder, decoder, autoencoder
