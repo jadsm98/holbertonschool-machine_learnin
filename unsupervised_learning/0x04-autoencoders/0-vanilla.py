c64bcdf30d86292c5e14286c1051c2420d204fe6
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-22 14:15:43 +0200

    vanilla

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -10,7 +10,8 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     encoder = keras.Sequential()
     for i, layer in enumerate(hidden_layers):
         if i == 0:
-            encoder.add(keras.layers.Dense(layer, input_shape=(input_dims,), activation='relu'))
+            encoder.add(keras.layers.Dense(layer, input_shape=(input_dims,),
+                                           activation='relu'))
         else:
             encoder.add(keras.layers.Dense(layer, activation='relu'))
     encoder.add(keras.layers.Dense(latent_dims, activation='relu'))
@@ -18,10 +19,12 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     decoder = keras.Sequential()
     for i in range(l, -1, -1):
         if i == l:
-            decoder.add(keras.layers.Dense(hidden_layers[i], input_shape=(latent_dims,),
+            decoder.add(keras.layers.Dense(hidden_layers[i],
+                                           input_shape=(latent_dims,),
                                            activation='relu'))
         else:
-            decoder.add(keras.layers.Dense(hidden_layers[i], activation='relu'))
+            decoder.add(keras.layers.Dense(hidden_layers[i],
+                                           activation='relu'))
     decoder.add(keras.layers.Dense(input_dims, activation='sigmoid'))
 
     inp = keras.Input(shape=(input_dims,))
