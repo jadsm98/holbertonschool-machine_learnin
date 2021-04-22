bc98ca76d68a27b645c8289fe6771cd5302a0a9a
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-22 15:15:09 +0200

    sparse

--- unsupervised_learning/0x04-autoencoders/1-sparse.py
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -7,23 +7,22 @@ import tensorflow.keras as keras
 
 def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     """function"""
-    l = len(hidden_layers) - 1
     inp1 = keras.Input((input_dims,))
     x = inp1
     for layer in hidden_layers:
-        x = keras.layers.Dense(layer, activation='relu')(x)
+        x = keras.layers.Dense(layer, activation="relu")(x)
     L1 = keras.regularizers.l1(lambtha)
-    x = keras.layers.Dense(latent_dims, activation='relu',
+    x = keras.layers.Dense(latent_dims, activation="relu",
                            activity_regularizer=L1)(x)
     encoder = keras.Model(inputs=inp1, outputs=x)
     inp2 = keras.Input((latent_dims,))
     x = inp2
     for layer in hidden_layers[::-1]:
-        x = keras.layers.Dense(layer, activation='relu')(x)
-    x = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+        x = keras.layers.Dense(layer, activation="relu")(x)
+    x = keras.layers.Dense(input_dims, activation="sigmoid")(x)
     decoder = keras.Model(inputs=inp2, outputs=x)
     x = encoder(inp1)
     out = decoder(x)
     auto = keras.Model(inputs=inp1, outputs=out)
-    auto.compile(optimizer='adam', loss='binary_crossentropy')
+    auto.compile(optimizer="adam", loss="binary_crossentropy")
     return encoder, decoder, auto
