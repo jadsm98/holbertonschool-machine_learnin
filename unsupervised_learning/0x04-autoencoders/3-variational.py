d368be6acd6bbab3e49451338112410ba619dc7a
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-23 18:49:08 +0200

    VAE

--- unsupervised_learning/0x04-autoencoders/3-variational.py
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -15,6 +15,18 @@ def Sampling(args):
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """function"""
+
+    def kl_reconstruction_loss(true, pred):
+        """Loss reconstruction"""
+        reconstruction_loss = keras.losses.binary_crossentropy(model_input,
+                                                               outputs)
+        reconstruction_loss *= input_dims
+        exp = keras.backend.exp(z_log_sigma)
+        kl_loss = 1 + z_log_sigma - keras.backend.square(z_mean) - exp
+        kl_loss = keras.backend.sum(kl_loss, axis=-1)
+        kl_loss *= -0.5
+        return keras.backend.mean(reconstruction_loss + kl_loss)
+
     inp1 = keras.Input(shape=(input_dims,))
     x = inp1
     for layer in hidden_layers:
