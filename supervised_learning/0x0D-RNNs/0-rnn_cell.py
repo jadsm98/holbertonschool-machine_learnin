23abac934eb5c601470dc2c2d4f098585f9a70dd
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-04-29 09:27:36 +0200

    rnn cell

--- supervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -3,22 +3,26 @@
 
 
 import numpy as np
-from scipy.special import softmax
 
 
 class RNNCell:
     """RNN cell class"""
 
     def __init__(self, i, h, o):
+        """constructor"""
         self.Wh = np.random.normal(size=(i + h, h))
         self.Wy = np.random.normal(size=(h, o))
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
+    @staticmethod
+    def softmax(z):
+        """applies softmax activation"""
+        return np.exp(z)/np.sum(np.exp(z), axis=1).reshape((-1, 1))
+
     def forward(self, h_prev, x_t):
         """forward prop"""
         concat = np.hstack((h_prev, x_t))
         h_next = np.tanh(np.matmul(concat, self.Wh) + self.bh)
-        y = softmax(np.matmul(h_next, self.Wy) + self.by, axis=1)
+        y = self.softmax(np.matmul(h_next, self.Wy) + self.by)
         return h_next, y
-  
