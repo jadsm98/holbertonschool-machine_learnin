80d57046da21a53b8652010adbed944cdc85bfbd
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-05-11 20:11:45 +0200

    BOW

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -3,13 +3,13 @@
 
 
 import numpy as np
-import sklearn as sk
+import sklearn.feature_extraction as sk
 
 
 def bag_of_words(sentences, vocab=None):
     """function"""
 
-    vector = sk.feature_extraction.text.CountVectorizer(vocabulary=vocab)
+    vector = sk.text.CountVectorizer(vocabulary=vocab)
     emb = vector.fit_transform(sentences)
     embeddings = emb.toarray()
     features = vector.get_feature_names()
