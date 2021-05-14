84b19bb9564f3b3023c0148baaf8baf10736bb32
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-05-14 22:18:39 +0200

    N-gram

---
+++ supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""module"""
+import numpy as np
+
+
+def new_list(lista, N):
+    """function"""
+    return [lista[i:i+N] for i in range(len(lista)-N+1)]
+
+
+def ngram_bleu(references, sentence, n):
+    """function"""
+    N_sentence = (new_list(sentence, n))
+    N_references = []
+    for ref in references:
+        N_references.append(new_list(ref, n))
+    len_sen = len(N_sentence)
+    min = max(len(sentence), max(len(i) for i in references))
+    for ref in references:
+        if abs(len(ref) - len(sentence)) < min:
+            min = abs(len(ref) - len(sentence))
+            closest_len = len(ref)
+    sum_count_clip = 0
+    added = []
+    for word in N_sentence:
+        count_clip = 0
+        for ref in N_references:
+            if ref.count(word) > count_clip:
+                count_clip = ref.count(word)
+        if word not in added:
+            sum_count_clip += count_clip
+            added.append(word)
+    bp = np.exp(1 - closest_len / len(sentence))
+    if len_sen >= closest_len:
+        bp = 1
+    return bp * sum_count_clip / len_sen
