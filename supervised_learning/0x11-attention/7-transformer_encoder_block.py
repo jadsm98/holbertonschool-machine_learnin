6a7547953890c229bf67fa6d13f6ce0e74550f9b
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-05-21 18:06:42 +0200

    Encoder block

--- supervised_learning/0x11-attention/7-transformer_encoder_block.py
+++ supervised_learning/0x11-attention/7-transformer_encoder_block.py
@@ -5,6 +5,7 @@
 import tensorflow as tf
 MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
 
+
 class EncoderBlock(tf.keras.layers.Layer):
     def __init__(self, dm, h, hidden, rate=0.1):
         super(EncoderBlock, self).__init__()
@@ -16,13 +17,13 @@ class EncoderBlock(tf.keras.layers.Layer):
         self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.dropout1 = tf.keras.layers.Dropout(rate)
         self.dropout2 = tf.keras.layers.Dropout(rate)
-    
+
     def call(self, x, training, mask=None):
-        attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
+        attn_output, _ = self.mha(x, x, x, mask)
         attn_output = self.dropout1(attn_output, training=training)
-        attn_output = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
-        ffn_output = self.dense_hidden(attn_output)  # (batch_size, input_seq_len, d_model)
+        attn_output = self.layernorm1(x + attn_output)
+        ffn_output = self.dense_hidden(attn_output)
         ffn_output = self.dense_output(ffn_output)
         ffn_output = self.dropout2(ffn_output, training=training)
-        ffn_output = self.layernorm2(attn_output + ffn_output)  # (batch_size, input_seq_len, d_model)
+        ffn_output = self.layernorm2(attn_output + ffn_output)
         return ffn_output
