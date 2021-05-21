4e2fc164376b18252ab154085198ee2b0ab26b41
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-05-21 18:26:25 +0200

    Encoder

--- supervised_learning/0x11-attention/9-transformer_encoder.py
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -8,14 +8,16 @@ EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 class Encoder(tf.keras.layers.Layer):
     """class"""
-    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
+    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
+                 drop_rate=0.1):
         """constructor"""
         super(Encoder, self).__init__()
         self.dm = dm
         self.N = N
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
+                       for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
@@ -23,7 +25,8 @@ class Encoder(tf.keras.layers.Layer):
         seq_len = x.shape.as_list()[1]
         x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
-        x += tf.convert_to_tensor(self.positional_encoding[:seq_len].reshape((1, seq_len, -1)), dtype=tf.float32)
+        pos_enc = self.positional_encoding[:seq_len].reshape((1, seq_len, -1))
+        x += tf.convert_to_tensor(pos_enc, dtype=tf.float32)
         x = self.dropout(x, training=training)
         for i in range(self.N):
             x = self.blocks[i](x, training, mask)
