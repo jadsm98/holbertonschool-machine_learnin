46e3bd9a655885e9e4e109e5767412875cf61228
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-05-21 18:18:38 +0200

    RNN encoder

--- supervised_learning/0x11-attention/0-rnn_encoder.py
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -5,24 +5,20 @@ import tensorflow as tf
 
 
 class RNNEncoder(tf.keras.layers.Layer):
-    """class"""
-
     def __init__(self, vocab, embedding, units, batch):
-        """constructor"""
         super(RNNEncoder, self).__init__()
         self.batch = batch
         self.units = units
-        self.embedding = tf.keras.layers.Embedding(input_dim=vocab,
-                                                   output_dim=embedding)
-        self.gru = tf.keras.layers.GRU(units, kernel_initializer='glorot_uniform',
-                                       return_sequences=True, return_state=True)
-
-    def initialize_hidden_state(self):
-        """method"""
-        return tf.zeros(shape=(self.batch, self.units))
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = tf.keras.layers.GRU(self.units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
 
     def call(self, x, initial):
-        """method"""
-        out = self.embedding(x)
-        output, hidden = self.gru(out, initial_state=initial)
-        return output, hidden
+        x = self.embedding(x)
+        outputs, hidden = self.gru(x, initial_state=initial)
+        return outputs, hidden
+
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch, self.units))
