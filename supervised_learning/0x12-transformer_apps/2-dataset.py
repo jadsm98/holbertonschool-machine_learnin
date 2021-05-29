c7434cb4f8c63a4cf7151188743f4312fe244d78
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-05-29 10:49:40 +0200

    dataset

---
+++ supervised_learning/0x12-transformer_apps/2-dataset.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""module"""
+
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+class Dataset:
+    """Class"""
+    def __init__(self):
+        """Initialize"""
+        datasets = tfds.load('ted_hrlr_translate/pt_to_en', as_supervised=True)
+        self.data_train = data_train.map(self.tf_encode)
+        self.data_valid = data_valid.map(self.tf_encode)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
+
+    def tokenize_dataset(self, data):
+        """Method"""
+        pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus((pt.numpy() for pt, en in data), 2**15)
+        en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus((en.numpy() for pt, en in data), 2**15)
+        return pt, en
+      
+    def encode(self, pt, en):
+        """Method"""
+        pt_start_index = self.tokenizer_pt.vocab_size
+        pt_end_index = pt_start_index + 1
+        en_start_index = self.tokenizer_en.vocab_size
+        en_end_index = en_start_index + 1
+        pt_tokens = [pt_start_index] + self.tokenizer_pt.encode(
+            pt.numpy()) + [pt_end_index]
+        en_tokens = [en_start_index] + self.tokenizer_en.encode(
+            en.numpy()) + [en_end_index]
+        return pt_tokens, en_tokens
+
+    def tf_encode(self, pt, en):
+        """Method"""
+        pt_encoded, en_encoded = tf.py_function(func=self.encode,
+                                                inp=[pt, en],
+                                                Tout=[tf.int64, tf.int64])
+        pt_encoded.set_shape([None])
+        en_encoded.set_shape([None])
+        return pt_encoded, en_encoded
