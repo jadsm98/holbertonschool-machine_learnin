a0f0e21a888d0582c90bebe0072296f53a50b144
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-05-29 10:43:13 +0200

    dataset

---
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""module"""
+
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+class Dataset:
+    """Class"""
+    def __init__(self):
+        """Initialize"""
+        datasets = tfds.load('ted_hrlr_translate/pt_to_en', as_supervised=True)
+        self.data_train = datasets['train']
+        self.data_valid = datasets['validation']
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
+
+    def tokenize_dataset(self, data):
+        """Method"""
+        pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus((pt.numpy() for pt, en in data), 2**15)
+        en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus((en.numpy() for pt, en in data), 2**15)
+        return pt, en
