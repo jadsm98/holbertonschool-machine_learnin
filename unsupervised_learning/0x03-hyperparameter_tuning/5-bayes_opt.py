cd9fd83c4d488f9b11ad3852afb9c253393d902a
jadsm98 <63211947+jadsm98@users.noreply.github.com>
2021-06-06 14:30:18 +0200

    bayes opt

--- unsupervised_learning/0x03-hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/5-bayes_opt.py
@@ -1,55 +1,61 @@
 #!/usr/bin/env python3
-"""module"""
-
-GP = __import__('2-gp').GaussianProcess
+"""
+Performs Bayesian optimization on a noiseless 1D Gaussian process
+"""
 import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    """class"""
-
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
-                 l=1, sigma_f=1, xsi=0.01, minimize=True):
-        """Initializer"""
+    """
+    Performs Bayesian optimization on a noiseless 1D Gaussian process
+    """
+    def __init__(self, f, X_init, Y_init,
+                 bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """
+        Class constructor
+        """
+        # black-box function to be optimized
         self.f = f
+
+        # instance of the class GaussianProcess
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape((ac_samples, 1))
+
+        # array containing all acquisition sample points,
+        # evenly spaced between min and max
+        # (1x50)
+        self.X_s = np.linspace(bounds[0], bounds[1], num=ac_samples)
+
+        # (50x1)
+        self.X_s = self.X_s.reshape(-1, 1)
+
+        # exploration-exploitation factor for acquisition
         self.xsi = xsi
+
+        # bool determining whether optimization should be performed for
+        # minimization (True) or maximization (False)
         self.minimize = minimize
 
     def acquisition(self):
-        """method"""
-        mue, sig = self.gp.predict(self.X_s)
-        if self.minimize:
-            f_best = np.amin(self.gp.Y)
-            upper = - mue + f_best - self.xsi
-            Z = np.where(sig > 0, upper/sig, 0)
-        else:
-            f_best = np.amax(self.gp.Y)
-            upper = mue - f_best - self.xsi
-            Z = np.where(sig > 0, upper / sig, 0)
-        cdf = norm.cdf(Z)
-        pdf = norm.pdf(Z)
-        eq = upper * cdf + sig * pdf
-        EI = np.where(sig > 0, eq, 0)
-        X_next = self.X_s[np.argmax(EI)]
-        return X_next, EI
+        """
+        Calculates the next best sample location
+        """
+        mu_s, sigma_s = self.gp.predict(self.X_s)
 
-    def optimize(self, iterations=100):
-        """method"""
-        xt = None
-        for n in range(iterations):
-            x_old = xt
-            xt, _ = self.acquisition()
-            yt = self.f(xt)
-            if xt == x_old:
-                self.gp.X = self.gp.X[:-2]
-                break
-            self.gp.update(xt, yt)
-        if self.minimize:
-            pos = np.argmin(self.gp.Y)
+        if self.minimize is True:
+            Y_s = np.min(self.gp.Y)
+            imp = Y_s - mu_s - self.xsi
+
         else:
-            pos = np.argmax(self.gp.Y)
-        y_best = self.gp.Y[pos]
-        x_best = self.gp.X[pos]
-        return x_best, y_best
+            Y_s = np.max(self.gp.Y)
+            imp = mu_s - Y_s - self.xsi
+
+        with np.errstate(divide='ignore'):
+            Z = imp / sigma_s
+            EI = imp * norm.cdf(Z) + sigma_s * norm.pdf(Z)
+            EI[sigma_s == 0.0] = 0.0
+
+        X_next = self.X_s[np.argmax(EI)]
+
+        return X_next, EI
